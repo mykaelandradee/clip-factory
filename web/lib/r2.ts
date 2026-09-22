08d98b80fc1b995a154846d475ffd8dd78dec1b8
Mykael Andrade <andrademykael@gmail.com>
2026-09-22 10:51:34 -0400

    Add direct R2 clip listing

--- web/lib/r2.ts
+++ web/lib/r2.ts
@@ -1,4 +1,4 @@
-import { DeleteObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { DeleteObjectCommand, ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
 
 function getR2Config() {
   const accountId = process.env.R2_ACCOUNT_ID;
@@ -17,6 +17,29 @@ function getR2Config() {
 export function getR2PublicClipUrl(jobId: string, file: string) {
   const { publicUrl } = getR2Config();
   return `${publicUrl}/jobs/${jobId}/${file}`;
+}
+
+export async function listR2ClipUrls(jobId: string) {
+  const { accountId, bucket, accessKeyId, secretAccessKey, publicUrl } = getR2Config();
+  const client = new S3Client({
+    region: "auto",
+    endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
+    credentials: { accessKeyId, secretAccessKey },
+  });
+
+  const response = await client.send(new ListObjectsV2Command({
+    Bucket: bucket,
+    Prefix: `jobs/${jobId}/`,
+  }));
+
+  return (response.Contents ?? [])
+    .map((object) => object.Key ?? "")
+    .filter((key) => /^jobs\/[^/]+\/clip-\d{2}\.mp4$/i.test(key))
+    .sort()
+    .map((key) => ({
+      file: key.split("/").pop() as string,
+      url: `${publicUrl}/${key}`,
+    }));
 }
 
 export async function deleteR2Clip(jobId: string, file: string) {
